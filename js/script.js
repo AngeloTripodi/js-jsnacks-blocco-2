7658beb0f6624022032360861f02c35c6c49f1f2
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-25 15:38:37 +0100

    add first Snack

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let arrayList = [];
+
+for (let i=0; i<6; i++) {
+    let addNumber = parseInt(prompt ('Inserisci un numero'));
+    if (addNumber % 2 == 1) {
+        arrayList.push(addNumber);
+    } 
+    console.log(arrayList);
+}
+

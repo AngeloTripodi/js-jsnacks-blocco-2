a82c4607ff9fb4cc6b486ad70210824c93ec5757
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-25 17:42:16 +0100

    add max numbers

--- js/script_3.js
+++ js/script_3.js
@@ -1,13 +1,19 @@
-let arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let arrayList = [1, 20, 3, 4, 50, 6, 7, 80, 9, 10, 11, 12, 13];
 
 let somma = 0;
 let avg = 0;
+let max = arrayList[0];
+
 for (let i=0; i<10; i++) {
    somma += arrayList[i];
-   
-} 
+
+   if (arrayList[i] > max){
+    max = arrayList[i];
+   } 
+   }
 
 avg = somma / 10;
 
 console.log(somma);
 console.log(avg);
+console.log(max);

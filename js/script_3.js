46fb4f70e53891bccbf2a1111a45c46feebd61f5
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-25 16:49:11 +0100

    add avg on Snack_3

--- js/script_3.js
+++ js/script_3.js
@@ -1,7 +1,11 @@
 let arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 let somma = 0;
+let avg = 0;
 for (let i=0; i<10; i++) {
    somma += arrayList[i];
+   avg = somma / 10;
+
 } 
     console.log(somma);
+    console.log(avg);

dbb7631c18ca4b1907a881f234d2b1bdeb5b6ea8
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-25 16:56:18 +0100

    fix for

--- js/script_3.js
+++ js/script_3.js
@@ -4,8 +4,10 @@ let somma = 0;
 let avg = 0;
 for (let i=0; i<10; i++) {
    somma += arrayList[i];
-   avg = somma / 10;
-
+   
 } 
-    console.log(somma);
-    console.log(avg);
+
+avg = somma / 10;
+
+console.log(somma);
+console.log(avg);

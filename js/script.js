aa7d5fb5d18c6780619b332a28b836c055b8cd32
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-25 16:43:13 +0100

    Add Snack_3

--- js/script.js
+++ js/script.js
@@ -7,4 +7,3 @@ for (let i=0; i<6; i++) {
     } 
     console.log(arrayList);
 }
-
---
+++ js/script_3.js
@@ -0,0 +1,7 @@
+let arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+let somma = 0;
+for (let i=0; i<10; i++) {
+   somma += arrayList[i];
+} 
+    console.log(somma);
